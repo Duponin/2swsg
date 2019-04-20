5656424efe43a20069380213a9ec5792675e6810
Duponin <pwet@dupon.in>
2019-04-20 07:14:14 +0200

    Put lib inside a function

--- lib/transform-to-html.sh
+++ lib/transform-to-html.sh
@@ -4,32 +4,35 @@
 
 set -e
 
-SOURCE="$1"
-DESTINATION="$2"
-TYPE="$3"
-
-source ./lib/yaml.sh
-create_variables ./config/site.yml
-
-if [[ -z $site_engine ]]; then exit 1; fi
-
-case "$site_engine" in
-pandoc)
-    create_variables ./config/engines/pandoc.yml
-    case "$TYPE" in
-        article)
-            CSS="--css=$article_css"
-            TEMPLATE="--templat=$article_template"
-            if [[ "$article_toc" = "true" ]]; then TOC="--toc"; fi
-            MATH="$article_math"
-        ;;
-        root_index)
-            CSS="--css=$root_index_css"
-            TEMPLATE="--templat=$root_index_template"
-            if [[ "$root_index_toc" = "true" ]]; then TOC="--toc"; fi
-            MATH="$root_index_math"
-        ;;
+function transform_to_html
+{
+    SOURCE="$1"
+    DESTINATION="$2"
+    TYPE="$3"
+    
+    source ./lib/yaml.sh
+    create_variables ./config/site.yml
+    
+    if [[ -z $site_engine ]]; then exit 1; fi
+    
+    case "$site_engine" in
+    pandoc)
+        create_variables ./config/engines/pandoc.yml
+        case "$TYPE" in
+            article)
+                CSS="--css=$article_css"
+                TEMPLATE="--templat=$article_template"
+                if [[ "$article_toc" = "true" ]]; then TOC="--toc"; fi
+                MATH="$article_math"
+            ;;
+            root_index)
+                CSS="--css=$root_index_css"
+                TEMPLATE="--templat=$root_index_template"
+                if [[ "$root_index_toc" = "true" ]]; then TOC="--toc"; fi
+                MATH="$root_index_math"
+            ;;
+        esac
+        pandoc $SOURCE $CSS $TEMPLATE $TOC $MATH -o $DESTINATION
+    ;;
     esac
-    pandoc $SOURCE $CSS $TEMPLATE $TOC $MATH -o $DESTINATION
-;;
-esac
+}
