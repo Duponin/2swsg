1550396201ac78361e2ac243a18e15dd92143f0d
Duponin <pwet@dupon.in>
2019-04-24 23:19:31 +0200

    Basic site index is done and point to articles

--- bin/generate-site-index.sh
+++ bin/generate-site-index.sh
@@ -1,15 +1,16 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+set -e
 
 # How it will work:
-# 1. List all articles
-# 2. Get metadatas
-# 3. Insert in index file
-# 4. Generate the index
+# 1. V List all articles
+# 2. V Get metadatas
+# 3. V Insert in index file
+# 4. V Generate the index
 # 5. Replace the old index by the new one
 
-source ./lib/yaml.sh
-source ./lib/transform-to-html.sh
-source ./lib/get-article-metadata.sh
+source lib/yaml.sh
+source lib/transform-to-html.sh
+source lib/get-article-metadata.sh
 
 # Load config file
 create_variables ./config/config.yml
@@ -26,6 +27,16 @@ function list_articles
 
 function update_index
 {
-    
+    NB_ARTICLES="$(wc -l $tmp_dir/articles-list.txt | awk '{print $1}')"
+    for ((i=1; i<NB_ARTICLES+1; i++))
+    do
+        create_variables "$(sed "${i}q;d" $tmp_dir/articles-list.txt)"
+        printf "## [%s](%s) \n" "$title" "$(sed "${i}q;d" $tmp_dir/articles-list.txt | sed "s/draft.md/${site_page}/g")" >> "$site_dir/index.md"
+        printf "``` ${abstract}  ```\n" >> "$site_dir/index.md"
+        printf "%s\n" "---" >> "$site_dir/index.md"
+    done
+    transform_to_html "${site_dir}/index.md" "${site_dir}/${site_page}" "root_index"
 }
 
+list_articles
+update_index
